2decedb916e3d0ad00b2974634897475cac4bc17
Natratz <nathan.r.a.schultz@gmail.com>
2025-02-20 23:47:22 -0500

    Improved the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
 export default function Navbar() {
     return (
         <div>
-            <p>This is my navbar!</p>
-            <p>LOVE EVERYONE</p>
+            <div className="absolute top-4 left-8 flex items-center space-x-6">
+                <p>This is my navbar!</p>
+                <p>Here's a second navbar element</p>
+                <p>LOVE EVERYONE</p>
+            </div>
         </div>
     );
 }
